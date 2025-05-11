c9bb70ea1b60e1d0633fbd911dacf72d5dc53134
gsmj <ykpauneu@gmail.com>
2025-05-12 00:17:25 +0300

    регистрация / авторизация

--- python/auth/auth.py
+++ python/auth/auth.py
@@ -5,6 +5,12 @@ from ..color_consts import (
     HIGHLIGHT_HEX,
 )
 from . import dialogs
+from ..selection.skin_selection import SelectionUI
+
+DEFAULT_SKINS: dict[int, list[int]] = {
+    0: [78, 79, 134, 135, 137, 212, 230, 36, 37, 67, ],
+    1: [64, 75, 77, 85, 90, 226, 233, ]
+}
 
 
 @Player.on_connect
@@ -19,9 +25,24 @@ def on_player_connect_auth(player: Player) -> None:
         f"Добро пожаловать на сервер {HIGHLIGHT_HEX}Merge RolePlay{WHITE_HEX}!"
     )
     dialogs.show_register_dialog(player)
+    return
 
 
 @Player.on_request_class
 @Player.using_registry
 def on_player_request_class(player: Player, class_id: int) -> None:
-    player.kick_if_not_logged()
+    if not player.is_choosing_skin:
+        player.kick_if_not_logged()
+
+    player.spawn()
+
+
+@Player.on_spawn
+@Player.using_registry
+def on_player_spawn_auth(player: Player) -> None:
+    print("spawn")
+    if player.is_choosing_skin:
+        SelectionUI.enable_selection(player, DEFAULT_SKINS[player.sex])
+        return
+
+    ...
--- python/auth/dialogs.py
+++ python/auth/dialogs.py
@@ -5,9 +5,56 @@ from ..color_consts import (
     HIGHLIGHT_HEX,
     WHITE_HEX,
     RED_HEX,
-    DARK_GREEN_HEX,
 )
-from samp import DIALOG_STYLE_INPUT
+from samp import DIALOG_STYLE_INPUT, DIALOG_STYLE_MSGBOX # type: ignore
+
+RULES_PAGE_ONE: str = f"""\
+{WHITE_HEX}1. {HIGHLIGHT_HEX}Игровой процесс.{WHITE_HEX}
+Запрещено:
+• Использование любых программ, читов, скриптов дающее преимущество в игре
+• Использование багов
+• Убивать игроков на спавне (Место возрождения, базы организации, выход из дома)
+• Убивать игроков при помощи транспорта (Давить).
+• Убийство игроков без причины (DM - Death Match)
+• Мошенничество (развод игроков на деньги, вымагательство и т.д)
+• Вымогательство паролей от аккаунта.
+• Выдача себя за членов администрации.
+
+2. {HIGHLIGHT_HEX}Ник в игре.{WHITE_HEX}
+(сменить ник можно через /mm >> Сменить ник)
+• Ник должен состоять из Имя_Фамилия с заглавных букв.
+Запрещено:
+• Использовать чужие (Уже зарегистрированные) ники.
+• Использовать ники, содержащие Нецензурные или оскорбительные слова.
+• Если вам отказали в смене ника, Значит нельзя.
+"""
+
+RULES_PAGE_TWO: str = f"""\
+{WHITE_HEX}3. {HIGHLIGHT_HEX}Чат сервера.{WHITE_HEX}
+ООС (Out Of Character) - это вся информация касающаяся реальной жизни.
+IC (In Character) - это все, что касается виртуального мира, игры.
+Запрещено:
+• Оскорбления или нецензурная речь.
+• Угрозы игрокам (Не относящиеся к игровому процессу).
+• Писать сообщения в верхнем регистре (Caps Lock).
+• Писать в чат объявлений сообщения не относящихся к RolePlay.
+• Использование ООС информации в ІС (Metagaming).
+• Писать одно и тоже сообщение слишком часто.
+• Обсуждать, критиковать действия администрации.
+• Реклама сторонних ресурсов.
+
+4. {HIGHLIGHT_HEX}Администрация.{WHITE_HEX}
+• Сообщать администрации о каких либо нарушениях (/report [id] [причина]).
+• Задать вопрос хелперу или администрации (/ask [вопрос])
+• Администрация самостоятельно выбирает меру наказания для каждого из случаев.
+• Запрещено препятствовать или мешать администрации в работе.
+
+5. {HIGHLIGHT_HEX}Торговля.{WHITE_HEX}
+• Запрещены любые денежные махинации.
+• Запрещена продажа / покупка чего либо, за реальные деньги.
+• Запрещен обмен внеигровых предметов в любой форме, на игровые.
+Запрещена продажа / передача аккаунтов.
+"""
 
 
 def show_register_dialog(player: Player) -> None:
@@ -24,6 +71,7 @@ def show_register_dialog(player: Player) -> None:
     ).show(player)
 
 
+@Player.using_registry
 def on_register_response(
     player: Player, response: int, list_item: int, input_text: str
 ) -> None:
@@ -39,3 +87,150 @@ def on_register_response(
         return
 
     player.password = input_text
+    show_register_rules_dialog(player)
+    return
+
+
+def show_register_rules_dialog(player: Player) -> None:
+    Dialog.create(
+        DIALOG_STYLE_MSGBOX,
+        "Правила сервера",
+        RULES_PAGE_ONE,
+        "Далее",
+        "Отмена",
+        on_response=on_rules_response,
+    ).show(player)
+
+
+@Player.using_registry
+def on_rules_response(
+    player: Player, response: int, list_item: int, input_text: str
+) -> None:
+        if not response:
+            player.kick_if_not_logged()
+            return
+
+        show_register_rules_dialog(player)
+        Dialog.create(
+            DIALOG_STYLE_MSGBOX,
+            "Правила сервера",
+            RULES_PAGE_TWO,
+            "Далее",
+            "Отмена",
+            on_response=on_rules_second_response,
+        ).show(player)
+
+
+# TODO: This is bad!!!! Rewrite!
+@Player.using_registry
+def on_rules_second_response(
+    player: Player, response: int, list_item: int, input_text: str
+) -> None:
+        if not response:
+            player.kick_if_not_logged()
+            return
+
+        show_email_register_dialog(player)
+        return
+
+
+def show_email_register_dialog(player: Player) -> None:
+    Dialog.create(
+        DIALOG_STYLE_INPUT,
+        "Регистрация | Email",
+        f"{WHITE_HEX}Введите адрес Вашей {HIGHLIGHT_HEX}"
+        f"электронной почты{WHITE_HEX}.\n"
+        "В случае утери доступа к аккаунту (потеря пароля, взлом) Вы сможете "
+        "воспользоваться восстановлением доступа к аккаунту.",
+        "Далее",
+        "Отмена",
+        on_response=on_email_register_response,
+    ).show(player)
+
+
+@Player.using_registry
+def on_email_register_response(
+    player: Player, response: int, list_item: int, input_text: str
+) -> None:
+    if not response:
+        player.kick_if_not_logged()
+        return
+
+    if len(input_text) < 3 or len(input_text) > 32:
+        player.send_error_message(
+            "Длина почты должна быть от 3 и до 32 символов"
+        )
+        show_email_register_dialog(player)
+        return
+
+    if "@" not in input_text:
+        player.send_error_message(
+            "Указанный адрес почты имеет неправильный формат"
+        )
+        show_email_register_dialog(player)
+        return
+
+    player.email = input_text
+    show_register_promocode_dialog(player)
+    return
+
+
+def show_register_promocode_dialog(player: Player) -> None:
+    Dialog.create(
+        DIALOG_STYLE_INPUT,
+        "Регистрация | Промокод",
+        f"{WHITE_HEX}Если у Вас есть {HIGHLIGHT_HEX}промокод{WHITE_HEX} "
+        "введите его в строке ниже.\n"
+        f"{HIGHLIGHT_HEX}Промокод{WHITE_HEX} можно получить от блогера, "
+        "другого игрока, при проведении мероприятия или акции.",
+        "Ввод",
+        "Пропустить",
+        on_response=on_register_promocode_response,
+    ).show(player)
+
+
+@Player.using_registry
+def on_register_promocode_response(
+    player: Player, response: int, list_item: int, input_text: str
+) -> None:
+    if not response:
+        show_register_sex_dialog(player)
+        return
+
+    if len(input_text) < 3 or len(input_text) > 12:
+        player.send_error_message(
+            "Длина промокода должна быть от 3 и до 12 символов"
+        )
+        show_register_promocode_dialog(player)
+        return
+
+    if "#" not in input_text:
+        player.send_error_message(
+            "Введите актуальный промокод"
+        )
+        show_register_promocode_dialog(player)
+        return
+
+    show_register_sex_dialog(player)
+    return
+
+
+def show_register_sex_dialog(player: Player) -> None:
+    Dialog.create(
+        DIALOG_STYLE_MSGBOX,
+        "Регистрация | Пол",
+        f"{WHITE_HEX}Выберите пол",
+        "Мужской",
+        "Женский",
+        on_response=on_register_sex_response,
+    ).show(player)
+
+
+@Player.using_registry
+def on_register_sex_response(
+    player: Player, response: int, list_item: int, input_text: str
+) -> None:
+    player.sex = not response
+    player.is_choosing_skin = True
+    player.toggle_spectating(False)
+    return
