4703b533ff51c85dd611b71fae25c57b96e0fbb8
gsmj <ykpauneu@gmail.com>
2025-05-17 14:22:07 +0300

    добавлена анимация при разговоре

--- python/roleplay/events.py
+++ python/roleplay/events.py
@@ -1,9 +1,17 @@
 from ..player import Player
+from pysamp.timer import set_timer
+from samp import PLAYER_STATE_ONFOOT
 
 
 @Player.on_text
 @Player.using_registry
 def on_player_text(player: Player, text: str) -> None:
+    if player.get_state() == PLAYER_STATE_ONFOOT:
+        player.apply_animation(
+            "PED", "IDLE_chat", 4.1, 0, 1, 1, 1, 1
+        )
+        set_timer(player.clear_animations, 2000, False)
+
     player.set_chat_bubble(text, -1, 20.0, 10000)
     player.prox_detector(20.0, player.get_color(), text)
     return False
