65a28162007e282cfe54752cdff38c52bc08f366
gsmj <ykpauneu@gmail.com>
2025-05-17 12:29:04 +0300

    доделан спавн

--- python/auth/auth.py
+++ python/auth/auth.py
@@ -6,7 +6,6 @@ from ..color_consts import (
 )
 from . import dialogs
 from ..selection.skin_selection import SelectionUI
-from pysamp.callbacks import registry
 
 DEFAULT_SKINS: dict[int, list[int]] = {
     0: [78, 79, 134, 135, 137, 212, 230, 36, 37, 67, ],
@@ -42,19 +41,20 @@ def on_player_request_class(player: Player, class_id: int) -> None:
 def on_player_spawn_auth(player: Player) -> None:
     if player.is_choosing_skin:
         SelectionUI.enable_selection(player, DEFAULT_SKINS[player.sex])
+        player.is_choosing_skin = False
         return
 
-    registry.register_callback("OnPlayerAuth", Player.on_auth)
-    registry.dispatch("OnPlayerAuth", player)
 
-
-@Player.on_auth
+@Player.on_select_skin
 @Player.using_registry
-def on_player_auth(player: Player) -> None:
-    player.set_pos(1153.9867, -1769.0027, 16.5938)
+def on_player_select_skin(player: Player) -> None:
+    if not player.is_logged:
+        player.is_logged = True
+        player.set_pos(1153.9867, -1769.0027, 16.5938)
+        player.set_facing_angle(360.0)
+        player.set_camera_behind()
 
 
-# 1153.9867, -1769.0027, 16.5938, 358.9192 // SpawnBomj
 # 1159.8640, -1768.3816, 16.5938, 272.7119 // BomjHp
 # -1964.3346, 137.9854, 27.6940, 89.4632 // MedSpawn
 # 2846.6091, 1290.9099, 11.3906, 90.8797 // RichSpawn
