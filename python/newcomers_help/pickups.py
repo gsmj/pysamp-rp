245c735da111445b11dfe0684bc0203b15321001
gsmj <ykpauneu@gmail.com>
2025-05-17 14:09:46 +0300

    добавлен пикап пополнения хп

---
+++ python/newcomers_help/pickups.py
@@ -0,0 +1,33 @@
+from pysamp import on_gamemode_init
+from pystreamer.dynamicpickup import DynamicPickup
+from ..player import Player
+
+LEVEL_HELP: int = 2
+
+
+@on_gamemode_init
+def on_ready() -> None:
+    print("Loaded: Newcomers pickups")
+    DynamicPickup.create(
+        1240,
+        23,
+        1159.8640,
+        -1768.3816,
+        16.5938,
+    )
+
+
+@DynamicPickup.on_player_pick_up
+@Player.using_registry
+def on_player_pickup_help(player: Player, pickup: DynamicPickup) -> None:
+    if player.level > LEVEL_HELP:
+        player.send_error_message(
+            "Вы можете восполнять своё здоровье до 2ого уровня"
+        )
+        return
+
+    if not player.check_cooldown(2.5):
+        player.send_error_message("Попробуйте позже")
+
+    player.set_health(100.0)
+    player.send_tip_message("Вы восполнили своё здоровье")
