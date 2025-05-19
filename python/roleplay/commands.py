c46b6cee1f0dc45d27dc2a2a87b7dcbfeb9f7b8f
gsmj <ykpauneu@gmail.com>
2025-05-19 16:59:48 +0300

    added commands: /me, /ame, /do, /coinflip, /s, /try

--- python/roleplay/commands.py
+++ python/roleplay/commands.py
@@ -1,1 +1,81 @@
+import random
+from ..color_consts import (
+    GREY,
+    ACTION,
+    HIGHLIGHT_HEX,
+    WHITE,
+    RED_HEX,
+    DARK_GREEN_HEX,
+)
 from ..player import Player
+
+
+@Player.command
+def me(player: Player, *message: str) -> None:
+    message = " ".join(message)
+    if len(message) == 0:
+        return player.send_error_message("Введите текст")
+
+    player.set_chat_bubble(message, ACTION, 20.0, 7500)
+    player.prox_detector(
+        ACTION, f"{player.get_name()} {message}"
+    )
+
+
+@Player.command
+def ame(player: Player, *message: str) -> None:
+    message = " ".join(message)
+    if len(message) == 0:
+        return player.send_error_message("Введите текст")
+
+    player.set_chat_bubble(message, ACTION, 20.0, 7500)
+
+
+@Player.command
+def do(player: Player, *message: str) -> None:
+    message = " ".join(message)
+    if len(message) == 0:
+        return player.send_error_message("Введите текст")
+
+    player.set_chat_bubble(message, ACTION, 20.0, 7500)
+    player.prox_detector(
+        GREY, f"{player.get_name()}: (( {message} ))"
+    )
+
+
+@Player.command
+def coinflip(player: Player) -> None:
+    coin = random.choice(("орёл", "решка"))
+    player.prox_detector(
+        ACTION, f"{player.get_name()} подбросил монетку"
+        f", выпал(а) {HIGHLIGHT_HEX}{coin}."
+    )
+
+
+@Player.command(aliases=("shout", ))
+def s(player: Player, *message: str) -> None:
+    message = " ".join(message)
+    if len(message) == 0:
+        return player.send_error_message("Введите текст")
+
+    player.set_chat_bubble(message, WHITE, 30.0, 7500)
+    player.prox_detector(
+        GREY, f"- {player.get_name()}[{player.id}] крикнул: {message}!",
+        max_range=30.0
+    )
+    player.apply_animation(
+        "ON_LOOKERS", "shout_01", 4.1, False, False, False, False, 0
+    )
+
+
+@Player.command(aliases=("try", ))
+def try_(player: Player, *message: str) -> None:
+    message = " ".join(message)
+    if len(message) == 0:
+        return player.send_error_message("Введите текст")
+
+    result = random.choice((f"{DARK_GREEN_HEX}удачно", f"{RED_HEX}неудачно"))
+    message = f"{result}"
+    player.prox_detector(
+        ACTION, f"{player.get_name()} {message} ({result})"
+    )
