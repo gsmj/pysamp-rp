d8de9baae3c17257f9fafe69d449f64b8700fbab
gsmj <ykpauneu@gmail.com>
2025-05-19 17:12:53 +0300

    fixed /do command, added /b (/ooc) command

--- python/roleplay/commands.py
+++ python/roleplay/commands.py
@@ -6,6 +6,7 @@ from ..color_consts import (
     WHITE,
     RED_HEX,
     DARK_GREEN_HEX,
+    ACTION_HEX,
 )
 from ..player import Player
 
@@ -39,7 +40,7 @@ def do(player: Player, *message: str) -> None:
 
     player.set_chat_bubble(message, ACTION, 20.0, 7500)
     player.prox_detector(
-        GREY, f"{player.get_name()}: (( {message} ))"
+        ACTION, f"{message} ({player.get_name()})"
     )
 
 
@@ -48,7 +49,7 @@ def coinflip(player: Player) -> None:
     coin = random.choice(("орёл", "решка"))
     player.prox_detector(
         ACTION, f"{player.get_name()} подбросил монетку"
-        f", выпал(а) {HIGHLIGHT_HEX}{coin}."
+        f", выпал(а) {HIGHLIGHT_HEX}{coin}{ACTION_HEX}."
     )
 
 
@@ -60,7 +61,7 @@ def s(player: Player, *message: str) -> None:
 
     player.set_chat_bubble(message, WHITE, 30.0, 7500)
     player.prox_detector(
-        GREY, f"- {player.get_name()}[{player.id}] крикнул: {message}!",
+        WHITE, f"{player.get_name()}[{player.id}] крикнул: {message}!",
         max_range=30.0
     )
     player.apply_animation(
@@ -75,7 +76,18 @@ def try_(player: Player, *message: str) -> None:
         return player.send_error_message("Введите текст")
 
     result = random.choice((f"{DARK_GREEN_HEX}удачно", f"{RED_HEX}неудачно"))
-    message = f"{result}"
+    message = f"{message} ({result})"
     player.prox_detector(
-        ACTION, f"{player.get_name()} {message} ({result})"
+        ACTION, f"{player.get_name()} {message})"
+    )
+
+
+@Player.command(aliases=("ooc", ))
+def b(player: Player, *message: str) -> None:
+    message = " ".join(message)
+    if len(message) == 0:
+        return player.send_error_message("Введите текст")
+
+    player.prox_detector(
+        GREY, f"{player.get_name()}: (( {message} ))"
     )
