147d24603279f25c99969eae6b2972ffb0fffc47
gsmj <ykpauneu@gmail.com>
2025-05-19 17:25:43 +0300

    Добавлен модуль server_consts.py

---
+++ python/server_consts.py
@@ -0,0 +1,17 @@
+from pysamp import on_gamemode_init, send_rcon_command
+
+SERVER_NAME: str = "Project Blue"
+SERVER_MODE: str = "BlueGM (v0.1.0-alpha)"
+SERVER_MAP: str = "San Andreas"
+SERVER_WEB: str = "https://www.youtube.com/watch?v=EEjtzZjhqkk"
+SERVER_DISCORD: str = "https://discord.gg/RDdJQ5KBuu"
+
+
+@on_gamemode_init
+def on_server_init() -> None:
+    send_rcon_command(f"name {SERVER_NAME}")
+    send_rcon_command(f"game.mode {SERVER_MODE}")
+    send_rcon_command(f"game.map {SERVER_MAP}")
+    send_rcon_command(f"website {SERVER_WEB}")
+    send_rcon_command(f"discord.invite {SERVER_DISCORD}")
+    print("Loaded: server_consts")
