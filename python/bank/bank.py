d523c8a194e0f46bdfe2102399b2e71802188192
gsmj <ykpauneu@gmail.com>
2025-05-22 20:20:08 +0300

    removed bank pickups, added textlabel

--- python/bank/bank.py
+++ python/bank/bank.py
@@ -1,7 +1,9 @@
 from dataclasses import dataclass
 from pystreamer.dynamicpickup import DynamicPickup
 from pystreamer.dynamicmapicon import DynamicMapIcon
-from .consts import BANK_WORLD_ID, BANK_INTERIOR_ID
+from pystreamer.dynamictextlabel import DynamicTextLabel
+from .consts import BANK_WORLD_ID
+from ..color_consts import HIGHLIGHT
 
 banks: dict[int, "Bank"] = {}
 
@@ -11,7 +13,6 @@ class Bank:
     name: str
     enter_pickup: DynamicPickup
     exit_pickup: DynamicPickup
-    pickups: tuple[DynamicPickup]
     position_on_enter: list[float]
     position_on_exit: list[float]
     map_icon_id: int
@@ -19,14 +20,24 @@ class Bank:
     angle_on_exit: float = 90.0
 
     def __post_init__(self) -> None:
+        x, y, z = *self.position_on_exit,
         DynamicMapIcon.create(
-            *self.position_on_exit,
+            x,
+            y,
+            z,
             self.map_icon_id,
             0,
             world_id=0,
             interior_id=0,
         )
-
+        DynamicTextLabel.create(
+            self.name,
+            HIGHLIGHT,
+            x,
+            y,
+            z + 0.5,
+            10.0,
+        )
 
 banks = {
     # 0: Bank(
@@ -57,33 +68,7 @@ banks = {
             78.5523,
             world_id=BANK_WORLD_ID,
         ),
-        (
-            DynamicPickup.create(
-                1212,
-                23,
-                2555.5708,
-                1341.0415,
-                78.5552,
-                world_id=BANK_WORLD_ID,
-            ),
-            DynamicPickup.create(
-                1212,
-                23,
-                2555.5698,
-                1339.5652,
-                78.5582,
-                world_id=BANK_WORLD_ID,
-            ),
-            DynamicPickup.create(
-                1212,
-                23,
-                2555.5715,
-                1338.0515,
-                78.5582,
-                world_id=BANK_WORLD_ID,
-            ),
-        ),
-        [2549.1804 ,1337.8145, 78.5523],
+        [2549.1804, 1337.8145, 78.5523],
         [2302.6252, -16.1454, 26.4844],
         52,
         angle_on_enter=270.0,
