aa9e6a0b28babdeb08c519ff6c2e0d095aafe7dd
gsmj <ykpauneu@gmail.com>
2025-05-22 20:20:17 +0300

    newcomers module fix

--- python/newcomers_help/pickups.py
+++ python/newcomers_help/pickups.py
@@ -3,25 +3,27 @@ from pystreamer.dynamicpickup import DynamicPickup
 from ..player import Player
 
 LEVEL_HELP: int = 2
-
+PICKUP_ID: int = None
 
 @on_gamemode_init
 def on_ready() -> None:
     print("Loaded: Newcomers pickups")
-    DynamicPickup.create(
+    global PICKUP_ID
+    PICKUP_ID = DynamicPickup.create(
         1240,
         23,
         1159.8640,
         -1768.3816,
         16.5938,
-    )
+    ).id
 
 
 @DynamicPickup.on_player_pick_up
 @Player.using_registry
 def on_player_pickup_help(player: Player, pickup: DynamicPickup) -> None:
-    if not player.check_cooldown(2.5):
-        player.send_error_message("Попробуйте позже")
+    if pickup.id == PICKUP_ID:
+        if not player.check_cooldown(2.5):
+            player.send_error_message("Попробуйте позже")
 
-    player.set_health(100.0)
-    player.send_tip_message("Вы восполнили своё здоровье")
+        player.set_health(100.0)
+        player.send_tip_message("Вы восполнили своё здоровье")
